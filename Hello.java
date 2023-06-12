c0106e8139d1e8f12fec7295555ad450fcff533b
tongchunpan <1809872737@qq.com>
2023-06-12 18:48:02 +0800

    第一次提交

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
-package PACKAGE_NAME;/**
+/**
  @author tongchunpan
  @Date 2023/6/12 18:45
- **/   public class Hello {
+ **/
+public class Hello {
+    /**
+     * 第一次提交
+     */
+    private int age;
+
 }
